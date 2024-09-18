329617100c5dd4e7ecd4e169ca8e91888cf0f3d6
Joshua Töpfer <joshua.toepfer@innoq.com>
2024-09-18 19:30:55 +0200

    intial spring boot project

---
+++ src/main/java/de/joshuatoepfer/bruno_demo/BrunoDemoApplication.java
@@ -0,0 +1,13 @@
+package de.joshuatoepfer.bruno_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrunoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrunoDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/joshuatoepfer/bruno_demo/BrunoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.joshuatoepfer.bruno_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrunoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
