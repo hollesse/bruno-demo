4057559103142e0fa46387119724d6d57fe8b7ed
Joshua Töpfer <joshua.toepfer@innoq.com>
2024-11-08 15:17:01 +0100

    added parameter to get only open todos

--- src/main/java/de/joshuatoepfer/bruno_demo/todo/TodoController.java
+++ src/main/java/de/joshuatoepfer/bruno_demo/todo/TodoController.java
@@ -1,9 +1,8 @@
 package de.joshuatoepfer.bruno_demo.todo;
 
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/todos")
@@ -23,26 +22,37 @@ public class TodoController {
 
   // Liste von Todos anzeigen
   @GetMapping
-  public List<Todo> getAllTodos() {
+  public List<Todo> getAllTodos(@RequestParam(name = "open", required = false) String open) {
+    if (open != null) {
+      return todoRepository.findByCompletedFalse();
+    }
     return todoRepository.findAll();
   }
 
   // Todo löschen
   @DeleteMapping("/{id}")
   public ResponseEntity<?> deleteTodo(@PathVariable Long id) {
-    return todoRepository.findById(id).map(todo -> {
-      todoRepository.delete(todo);
-      return ResponseEntity.ok().build();
-    }).orElse(ResponseEntity.notFound().build());
+    return todoRepository
+        .findById(id)
+        .map(
+            todo -> {
+              todoRepository.delete(todo);
+              return ResponseEntity.ok().build();
+            })
+        .orElse(ResponseEntity.notFound().build());
   }
 
   // Todo als erledigt markieren
   @PutMapping("/{id}/complete")
   public ResponseEntity<Todo> completeTodo(@PathVariable Long id) {
-    return todoRepository.findById(id).map(todo -> {
-      todo.setCompleted(true);
-      Todo updatedTodo = todoRepository.save(todo);
-      return ResponseEntity.ok(updatedTodo);
-    }).orElse(ResponseEntity.notFound().build());
+    return todoRepository
+        .findById(id)
+        .map(
+            todo -> {
+              todo.setCompleted(true);
+              Todo updatedTodo = todoRepository.save(todo);
+              return ResponseEntity.ok(updatedTodo);
+            })
+        .orElse(ResponseEntity.notFound().build());
   }
 }
--- src/main/java/de/joshuatoepfer/bruno_demo/todo/TodoRepository.java
+++ src/main/java/de/joshuatoepfer/bruno_demo/todo/TodoRepository.java
@@ -2,5 +2,9 @@ package de.joshuatoepfer.bruno_demo.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+  List<Todo> findByCompletedFalse();
 }
