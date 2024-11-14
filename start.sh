063e1931a84ea6df85b1a2764a2043b250b407d2
Joshua Töpfer <joshua.toepfer@innoq.com>
2024-11-14 08:58:17 +0100

    Switch auth method to basic-auth in start.sh
    Revert the authentication method from OAuth2 to basic-auth in `start.sh`. This change is likely for testing purposes or to match a specific deployment environment. Updated the `Bruno Demo` documentation to improve readability with added spaces and made minor formatting adjustments.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#export BRUNO_DEMO_AUTH_METHOD="basic-auth"
-export BRUNO_DEMO_AUTH_METHOD="oauth2"
+export BRUNO_DEMO_AUTH_METHOD="basic-auth"
+#export BRUNO_DEMO_AUTH_METHOD="oauth2"
 
 docker compose up --build
